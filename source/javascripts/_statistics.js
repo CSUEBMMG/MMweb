591e136fca701f3e2b234eac335ba689e8a017e7
ian <instructian@users.noreply.github.com>
2019-10-07 18:53:39 -0700

    Update _statistics.js

--- source/javascripts/_statistics.js
+++ source/javascripts/_statistics.js
@@ -18,3 +18,17 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     fbq('track', "PageView");
   });
 })(jQuery);
+
+
+(function(b,o,n,g,s,r,c){
+  if(b[s])return;b[s]={};
+  b[s].scriptToken="Xy0xODEyMjM4ODc1";
+  b[s].callsQueue=[];
+  b[s].api=function(){b[s].callsQueue.push(arguments);};
+  r=o.createElement(n);
+  c=o.getElementsByTagName(n)[0];
+  r.async=1;
+  r.src=g;
+  r.id=s+n;
+  c.parentNode.insertBefore(r,c);
+})(window,document,"script","//cdn.oribi.io/Xy0xODEyMjM4ODc1/oribi.js","ORIBI");
